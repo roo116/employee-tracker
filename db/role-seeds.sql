b1e327745773a1df6ac40f5de743b8ee87ff905b
roo116 <roo116@nc.rr.com>
2022-07-08 03:06:13 -0400

    created the seed files for all of the tables.  Took forevergit add -A

---
+++ db/role-seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO role (title, salary, department_id)
+VALUES
+('CEO', 1000000, 1),
+('CFO', 500000, 1),
+('VP', 300000, 1),
+('Regional Sales Manager', 80000, 4),
+('Sales Representative', 50000, 4),
+('Receptionist', 40000, 2),
+('Temporary', 30000, 2),
+('Supplier Relations', 50000, 5),
+('Warehouse', 45000, 5),
+('Human Resources Representative', 75000, 6),
+('Accountant', 90000, 3),
+('Customer Service', 40000, 4),
+('Quality Assurance', 40000, 5);
+
